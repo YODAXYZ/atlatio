e87d189372a2a770d8d3c4498c1d7506f86698cf
YODA <YODAXYZ>
2018-12-11 19:55:54 +0300

    First_commit

---
+++ desktop/src/com/atlatio/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.atlatio.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.atlatio.MainGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGameClass(), config);
+	}
+}
